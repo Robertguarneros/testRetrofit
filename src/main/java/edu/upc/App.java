cd13c3edfa8c0a148c6595a64255cdc07ddda3bf
Roberto <roberto.guarneros11@gmail.com>
2023-11-12 11:26:30 +0100

    Get tracks is working

--- src/main/java/edu/upc/App.java
+++ src/main/java/edu/upc/App.java
@@ -13,25 +13,23 @@ import retrofit2.http.Path;
 
 public class App 
 {
-    public static final String API_URL = "https://api.github.com";
+    public static final String API_URL = "http://localhost:8080/dsaApp/";//path defined in BASE_URI on API
+    //class song where a song will be stored
+    public static class Song {
+        public final String id;
+        public final String title;
+        public final String singer;
 
-    public static class Contributor {
-        public final String login;
-        public final int contributions;
-        public final String avatar_url;
-
-        public Contributor(String login, int contributions, String avatar_url) {
-            this.login = login;
-            this.contributions = contributions;
-            this.avatar_url = avatar_url;
+        public Song(String id, String title, String singer) {
+            this.id = id;
+            this.title = title;
+            this.singer = singer;
         }
     }
-
-    public interface GitHub {
-        @GET("/repos/{owner}/{repo}/contributors")
-        Call<List<Contributor>> contributors(
-                @Path("owner") String owner,
-                @Path("repo") String repo);
+    //Interface that will use get method to get the song list. Here use the path in swagger. In this case http://localhost:8080/dsaApp/tracks. Since we have the rest just add tracks
+    public interface TracksInterface {
+        @GET("tracks")
+        Call<List<Song>> songs();
     }
 
     public static void main( String[] args ) throws IOException
@@ -40,23 +38,23 @@ public class App
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
 
-        // Create a very simple REST adapter which points the GitHub API.
+        // Create a very simple REST adapter which points the API.
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(API_URL)
                 .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        // Create an instance of our GitHub API interface.
-        GitHub github = retrofit.create(GitHub.class);
+        // Create an instance of our tracks API interface.
+        TracksInterface tracksInterface = retrofit.create(TracksInterface.class);
 
-        // Create a call instance for looking up Retrofit contributors.
-        Call<List<Contributor>> call = github.contributors("RouteInjector", "route-injector");
+        // Create a call instance for looking up Retrofit songs.
+        Call<List<Song>> call = tracksInterface.songs();
 
         // Fetch and print a list of the contributors to the library.
-        List<Contributor> contributors = call.execute().body();
-        for (Contributor contributor : contributors) {
-            System.out.println(contributor.login + " (" + contributor.contributions + ") "+ contributor.avatar_url);
+        List<Song> songs = call.execute().body();
+        for (Song song : songs) {
+            System.out.println("Song ID: "+song.id+"\nTitle: "+ song.title+"\nSinger: " + song.singer+"\n");
         }
     }
 }
