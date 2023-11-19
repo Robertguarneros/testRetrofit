a44df391e65842840f1c7400be697b92c5aa462a
Roberto <roberto.guarneros11@gmail.com>
2023-11-19 18:42:00 +0100

    Asynchronous Retrofit For Project Finished

--- src/main/java/edu/upc/App.java
+++ src/main/java/edu/upc/App.java
@@ -3,6 +3,8 @@ package edu.upc;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -49,7 +51,7 @@ public class App {
         }
     }
 
-    public class StoreObject {
+    public static class StoreObject {
         String objectID;
         String articleName;
         int price;
@@ -91,7 +93,36 @@ public class App {
         Call<JsonObject> getStoreSize();
         @GET("getNumberOfUsers")
         Call<JsonObject> getNumberOfUsers();
-
+        @GET("{username}")
+        Call<User> getUser(@Path("username") String username);
+        @GET("getAllUsers")
+        Call<List<User>> getAllUsers();
+        @GET("getObjectListFromStore")
+        Call<List<StoreObject>> getAllObjectsFromStore();
+        @GET("getPlayedMatchesFromUser/{username}")
+        Call<List<Match>> getPlayedMatchesFromUser(@Path("username")String username);
+        @GET("getObjectInformation/{objectID}")
+        Call<StoreObject> getObject(@Path("objectID")String objectID);
+        @GET("getActiveMatch/{username}")
+        Call<Match> getActiveMatch(@Path("username")String username);
+        @POST("registerNewUser")
+        Call<Void> registerUser(@Body User user);
+        @POST("login")
+        Call<Void> loginUser(@Body User user);
+        @PUT("addItemToUser/{username}/{objectID}")
+        Call<Void> addItemToUser(@Path("username")String username,@Path("objectID")String objectID);
+        @PUT("createMatch/{username}")
+        Call<Void> createMatch(@Path("username")String username);
+        @GET("getLevelFromActiveMatch/{username}")
+        Call<JsonObject> getLevelFromActiveMatch(@Path("username")String username);
+        @GET("getMatchPointsFromActiveMatch/{username}")
+        Call<JsonObject> getMatchPointsFromActiveMatch(@Path("username")String username);
+        @PUT("nextLevel/{username}/{points}")
+        Call<Void> nextLevel(@Path("username")String username,@Path("points")int points);
+        @PUT("endMatch/{username}")
+        Call<Void> endMatch(@Path("username")String username);
+        @POST("addObjectToStore")
+        Call<Void> addObjectToStore(@Body StoreObject Object);
     }
 
     public static void main(String[] args) throws IOException {
@@ -151,6 +182,300 @@ public class App {
             @Override
             public void onFailure(Call<JsonObject> call, Throwable t) {
                 System.out.println("Error: " + t.getMessage());
+            }
+        });
+        //Method to obtain one user by username
+        String getThisUsername = "robertoguarneros11";//Enter ID here
+        Call<User> callGetUsername = pixelRushInterface.getUser(getThisUsername);
+        callGetUsername.enqueue(new Callback<User>(){
+            @Override
+            public void onResponse(Call<User> call,Response<User> response){
+                if(response.isSuccessful()){
+                    User usernameFound = response.body();
+                    System.out.println("User found:\nUsername: " + usernameFound.username + "\nName: " + usernameFound.name + "\nMail: " + usernameFound.mail + "\n");
+
+                }else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+            }
+        });
+        //Method to get all users
+        Call<List<User>> callGetAllUsers = pixelRushInterface.getAllUsers();
+        callGetAllUsers.enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                if (response.isSuccessful()) {
+                    List<User> users = response.body();
+                    for (User user : users) {
+                        System.out.println("Name: " + user.username + "\nSurname: " + user.surname + "\nusername: " + user.age+ "\n");
+                    }
+                } else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+            }
+        });
+        //Method to get all objects available in the store
+        Call<List<StoreObject>> callGetAllStoreObjects = pixelRushInterface.getAllObjectsFromStore();
+
+        callGetAllStoreObjects.enqueue(new Callback<List<StoreObject>>() {
+            @Override
+            public void onResponse(Call<List<StoreObject>> call, Response<List<StoreObject>> response) {
+                if (response.isSuccessful()) {
+                    List<StoreObject> objects = response.body();
+                    for (StoreObject object : objects) {
+                        System.out.println("Article Name: " + object.articleName + "\nObject ID: " + object.objectID + "\nDescription: " + object.description + "\n");
+                    }
+                } else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+            @Override
+            public void onFailure(Call<List<StoreObject>> call, Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+            }
+        });
+        //Method to get all played matches from user
+        String getMatchesForThisUser = "robertoguarneros11";//Enter ID here
+        Call<List<Match>> callGetPlayedMatchesFromUser = pixelRushInterface.getPlayedMatchesFromUser(getMatchesForThisUser);
+        callGetPlayedMatchesFromUser.enqueue(new Callback<List<Match>>() {
+            @Override
+            public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
+                if (response.isSuccessful()) {
+                    List<Match> matches = response.body();
+                    for (Match match : matches) {
+                        System.out.println("Match: \nUsername: " + match.username + "\nCurrent Level: " + match.currentLVL + "\nTotal Points: " + match.totalPoints + "\n");
+                    }
+                } else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Match>> call, Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+            }
+        });
+        //Method to obtain object by ID
+        String getThisObject = "123";//Enter ID here
+        Call<StoreObject> callGetObject = pixelRushInterface.getObject(getThisObject);
+        callGetObject.enqueue(new Callback<StoreObject>(){
+            @Override
+            public void onResponse(Call<StoreObject> call,Response<StoreObject> response){
+                if(response.isSuccessful()){
+                    StoreObject objectFound = response.body();
+                    System.out.println("Object found:\nArticle Name: " + objectFound.articleName + "\nObject ID: " + objectFound.objectID + "\nDescription: " + objectFound.description + "\n");
+
+                }else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+            @Override
+            public void onFailure(Call<StoreObject> call, Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+            }
+        });
+        //Method to obtain current match of username
+        String getMatchForThisUser = "titi";//Enter ID here
+        Call<Match> callGetActiveMatch = pixelRushInterface.getActiveMatch(getMatchForThisUser);
+        callGetActiveMatch.enqueue(new Callback<Match>(){
+            @Override
+            public void onResponse(Call<Match> call,Response<Match> response){
+                if(response.isSuccessful()){
+                    Match match = response.body();
+                    System.out.println("Current Match: \nUsername: " + match.username + "\nCurrent Level: " + match.currentLVL + "\nTotal Points: " + match.totalPoints + "\n");
+
+                }else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+            @Override
+            public void onFailure(Call<Match> call, Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+            }
+        });
+        //Register
+        User registerUser = new User("PruebaUsername","password","mail","name","surname",40);
+        Call<Void> callRegister = pixelRushInterface.registerUser(registerUser);
+        callRegister.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    System.out.println("User registered successfully");
+                } else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                System.out.println("Error: "+t.getMessage());
+            }
+        });
+        //Login
+        User login = new User("robertoguarneros11","123","Roberto","Guarneros","roberto@gmail.com",22);
+        Call<Void> callLogin = pixelRushInterface.loginUser(login);
+        callLogin.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    System.out.println("User login successful");
+                } else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                System.out.println("Error: "+t.getMessage());
+            }
+        });
+        //Add item to this user
+        String addToThisUser="robertoguarneros11";
+        String addThisObjectID="123";
+        Call<Void> callAddItemToUser = pixelRushInterface.addItemToUser(addToThisUser,addThisObjectID);
+        callAddItemToUser.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    System.out.println("Item added successfully");
+                } else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                System.out.println("Error: "+t.getMessage());
+            }
+        });
+        //Create Match
+        String createMatchFor="robertoguarneros11";
+        Call<Void> callCreateMatch = pixelRushInterface.createMatch(createMatchFor);
+        callCreateMatch.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    System.out.println("Match Created successfully");
+                } else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                System.out.println("Error: "+t.getMessage());
+            }
+        });
+        // Method to get level from active match
+        String getLevelFor="titi";
+        Call<JsonObject> callGetLevelFromActiveMatch = pixelRushInterface.getLevelFromActiveMatch(getLevelFor);
+        callGetLevelFromActiveMatch.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                if (response.isSuccessful()) {
+                    JsonObject jsonResponse = response.body();
+                    if (jsonResponse != null) {
+                        int level = jsonResponse.get("level").getAsInt();
+                        System.out.println("Current level for user: " +getLevelFor+" is " +level);
+                    } else {
+                        System.out.println("Response body is null");
+                    }
+                } else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+            }
+        });
+        // Method to get points from active match
+        String getPointsFor="titi";
+        Call<JsonObject> callGetPointsFromActiveMatch = pixelRushInterface.getMatchPointsFromActiveMatch(getPointsFor);
+        callGetPointsFromActiveMatch.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                if (response.isSuccessful()) {
+                    JsonObject jsonResponse = response.body();
+                    if (jsonResponse != null) {
+                        int points = jsonResponse.get("matchPoints").getAsInt();
+                        System.out.println("Current points for user: " +getPointsFor+" is " +points);
+                    } else {
+                        System.out.println("Response body is null");
+                    }
+                } else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+            }
+        });
+        // Method to change level for active match
+        String changeLevelFor="titi";
+        int points=100;
+        Call<Void> callChangeLevel = pixelRushInterface.nextLevel(changeLevelFor, points);
+        callChangeLevel.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    System.out.println("\t\n" +
+                            "Level changed successfully");
+                } else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                System.out.println("Error: "+t.getMessage());
+            }
+        });
+        // Method to change end active match
+        String endMatch="titi";
+        Call<Void> callEndMatch = pixelRushInterface.endMatch(endMatch);
+        callEndMatch.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    System.out.println("\t\n"+ "Match ended successfully");
+                } else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                System.out.println("Error: "+t.getMessage());
+            }
+        });
+        //Method add object to store
+        StoreObject newObject = new StoreObject("333","pruebaNombre",50,"description here");
+        Call<Void> callAddObjectToStore = pixelRushInterface.addObjectToStore(newObject);
+        callAddObjectToStore.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    System.out.println("New object added successfully");
+                } else {
+                    System.out.println("Error: " + response.code() + " " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                System.out.println("Error: "+t.getMessage());
             }
         });
     }
